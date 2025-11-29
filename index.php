d47104ae16cdbe1cb2ff38971390feada5a5b941
Evandro Lucas Anacleto Batista <evandrolucasanacletob@gmail.com>
2025-11-29 15:41:30 -0300

    pquena alteracao

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ include('app/conexao/MySQL.php');
 if (isset($_POST['email']) || isset($_POST['senha'])) {
 
     if (strlen($_POST['email']) == 0) {
-        echo "Preencha seu e-mail";
+        echo "Preencha seu e-mail:";
     } else if (strlen($_POST['senha']) == 0) {
-        echo "Preencha sua senha";
+        echo "Preencha sua senha:";
     } else {
 
         $email = $mysqli->real_escape_string($_POST['email']);
